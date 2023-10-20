55ebea7627b21e39bd58781d638b1e98ffb27cca
Adailson <adayllson.almeida@gmail.com>
2023-10-20 15:07:29 -0300

    configurando a DTO

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,18 +1,29 @@
 package com.example.cardapio.controller;
 
+//import org.hibernate.mapping.List;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+
+
 @RestController
 @RequestMapping("food")
 public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
     
     //Notação que retorna os itens do endpoint "food"
     @GetMapping
-    public void getAll(){
+    public List<FoodResponseDTO> getAll(){
         
-        
+        List<FoodResponseDTO> foodList = (List<FoodResponseDTO>) repository.findAll().stream().map(FoodResponseDTO::new);
+        return foodList;
         
     }
 }
