a9a0ab3c9ce9cf232994d4190bef54afb5c38070
Adailson <adayllson.almeida@gmail.com>
2023-10-24 15:02:07 -0300

    Construindo os cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { FoodData } from './Interface/FoodData';
 import './App.css'
+import { Card } from './components/card/card';
 
 function App() {
-  
+  const data: FoodData[] = [];
 
   return (
     
       <div className="container">
         <h1>Cardápio</h1>
         <div className="card-grid">
-
+          {data.map(_foodData => <Card 
+          price={_foodData.price} 
+          title={_foodData.title} 
+          image={_foodData.image}
+          />
+          )}
         </div>
       </div>
       
